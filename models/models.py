e1cb1ed44fa2fc4e58b7960abd556b182b54f207
Hammad Usmani <ham.u@live.com>
2018-02-07 12:16:28 -0500

    Fix bugs in error database module

--- models/models.py
+++ models/models.py
@@ -58,20 +58,25 @@ class models :
       # Data starts at line 9 
       for line in lines[9:] :
           line = line.split()
-          
+        
           # Identify atlantic storm date, storm id, associated sample sizes, latitude and longitude, and windspeed
-          timestamp = datetime.datetime.strptime(line[0], "%d-%m-%Y/%H:%M:%S").timetuple()
+          timestamp = datetime.datetime.strptime(line[0], "%d-%m-%Y/%H:%M:%S")
           storm_id = line[1]
           sample_sizes = {"F012": float(line[2]), "F024": float(line[3]),"F036": float(line[4]), "F048": float(line[5]), "F072": float(line[6]), "F096": float(line[7]), "F120": float(line[8]), "F144": float(line[9]), "F168": float(line[10])} 
           latitude = float(line[11])
           longitude = float(line[12])
           wind_speed = int(line[13])
+        
                     
           # Iterate through model forecast track and intensity errors 
           for i in range(len(model_names)) :
-            intensity_forecast = zip([timestamp, timestamp + timedelta(hours = 12), timestamp + timedelta(hours = 24), timestamp + timedelta(hours = 36), timestamp + timedelta(hours = 48), timestamp + timedelta(hours = 72), timestamp + timedelta(hours = 96), timestamp + timedelta(hours = 120), timestamp + timedelta(hours = 144), timestamp + timedelta(hours = 168)], [None if x == "-9999.0" else float(x) for x in line[14 + (20 * i) : 24 + (20 * i)]])
-            track_forecast = zip([timestamp, timestamp + timedelta(hours = 12), timestamp + timedelta(hours = 24), timestamp + timedelta(hours = 36), timestamp + timedelta(hours = 48), timestamp + timedelta(hours = 72), timestamp + timedelta(hours = 96), timestamp + timedelta(hours = 120), timestamp + timedelta(hours = 144), timestamp + timedelta(hours = 168)], [None if x == "-9999.0" else float(x) for x in line[24 + (20 * i) : 34 + (20 * i)]])
-            self.models[model_names[i]].storm[storm_id] = {
+            intensity_forecast = dict(list(zip([timestamp, timestamp + timedelta(hours = 12), timestamp + timedelta(hours = 24), timestamp + timedelta(hours = 36), timestamp + timedelta(hours = 48), timestamp + timedelta(hours = 72), timestamp + timedelta(hours = 96), timestamp + timedelta(hours = 120), timestamp + timedelta(hours = 144), timestamp + timedelta(hours = 168)], [None if x == "-9999.0" else float(x) for x in line[14 + (20 * i) : 24 + (20 * i)]])))
+            track_forecast = dict(list(zip([timestamp, timestamp + timedelta(hours = 12), timestamp + timedelta(hours = 24), timestamp + timedelta(hours = 36), timestamp + timedelta(hours = 48), timestamp + timedelta(hours = 72), timestamp + timedelta(hours = 96), timestamp + timedelta(hours = 120), timestamp + timedelta(hours = 144), timestamp + timedelta(hours = 168)], [None if x == "-9999.0" else float(x) for x in line[24 + (20 * i) : 34 + (20 * i)]])))
+            
+            # Add forecast to model and storm, initialize if storm id does not exist
+            if storm_id not in self.models[model_names[i]].storm.keys() :
+                self.models[model_names[i]].storm[storm_id] = dict()
+            self.models[model_names[i]].storm[storm_id].update({
               timestamp : {
                 "sample_sizes" : sample_sizes,
                 "lat" : latitude,
@@ -80,4 +85,4 @@ class models :
                 "intensity_forecast" : intensity_forecast,
                 "track_forecast" : track_forecast,
               }
-            }
+            })
