40f233a020dbb30d1a1d50e00c0179db45672b52
Hammad Usmani <ham.u@live.com>
2025-07-16 01:33:26 +0000

    Create initial draft for prompt features

---
+++ src/src/hurricane_net/prompt.py
@@ -0,0 +1,15 @@
+import logging
+from langchain_core.prompts import PromptTemplate
+
+class Prompts
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format="hurricane_net %(asctime)s - %(levelname)s - %(name)s - %(message)s")
+    def __init__(self):
+        self.daily_report = self.prompt_daily_report()
+    
+    def load_prompt(self, path):
+        with open(path) as f:
+            return f.read()
+    
+    def prompt_daily_report(self, path = 'prompts/daily_report.txt'):
+        return self.load_prompt(path)
+prompts = Prompts()
